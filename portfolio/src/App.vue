5fa95a6bc187aefda30e16ad5a4e9baf69ff6417
JorgoVdP <Jorgo.vandeparrerstudent.pxl.begit config --global user.email Jorgo.vandeparrerstudent.pxl>
2023-11-11 09:01:54 +0100

    added navbar component

--- portfolio/src/App.vue
+++ portfolio/src/App.vue
@@ -1,14 +1,18 @@
 <template>
+  <AppNavBar></AppNavBar>
   <HomePageView></HomePageView>
 </template>
 
 <script>
 import HomePageView from './Views/HomePageView.vue';
+import AppNavBar from './Components/AppNavBar.vue';
+
 
 export default {
   name: 'App',
   components: {
     HomePageView,
+    AppNavBar,
   }
 }
 </script>
---
+++ portfolio/src/Components/AppNavBar.vue
@@ -0,0 +1,25 @@
+<template>
+    <div id="main-wrapper">
+      <p>temp empty</p>
+    </div>
+  </template>
+  
+  <script>
+  
+  export default {
+    name: 'AppNavbar',
+    components: {
+      
+    }
+  }
+  </script>
+  
+  <style scoped>
+  #main-wrapper{
+      height: 20%;
+      width: 100%;
+      display: flex;
+      justify-content: center;
+      align-items: center;    
+  }
+  </style>
